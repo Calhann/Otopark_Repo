9ce0033d19f1e918ae9644c56fc6167462d09102
Metehan Çalhan <metehan.calhan1@gmail.com>
2025-11-25 07:36:43 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace Otopark
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string pythonPath = @"C:\Python312\python.exe"; // Gerçek python.exe yolu
+            string scriptPath = @"C:\Users\Emre\Desktop\Otopark\Otopark\opencvdeneme.py";
+
+            var psi = new ProcessStartInfo()
+            {
+                FileName = pythonPath,
+                Arguments = $"\"{scriptPath}\"", // Script yolunu tırnak içine al
+                UseShellExecute = false,          // Çıktıları almak için false
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true        ,
+                WorkingDirectory = @"C:\Users\Emre\Desktop\Otopark\Otopark" // Scriptin olduğu klasör
+
+            };
+
+            try
+            {
+                using (var process = Process.Start(psi))
+                {
+         
+                    process.WaitForExit();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+    }
+}
